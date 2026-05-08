62d6fdbcf5f9d99c363ffde9be7af7116d538f83
salkfdjasldfkas <89749216+salkfdjasldfkas@users.noreply.github.com>
2026-05-08 14:30:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const http = require('http');
 const WebSocket = require('ws');
-const net = require('net');
 
 const TARGET_HOST = 'sfra.sploop.io';
-const TARGET_PORT = 443;
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('ok');
+});
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (clientWs) => {
-    const targetWs = new WebSocket(`wss://${TARGET_HOST}`, { rejectUnauthorized: false });
-    
+    const targetWs = new WebSocket(`wss://${TARGET_HOST}`, {
+        rejectUnauthorized: false
+    });
+
     targetWs.on('open', () => {
         clientWs.on('message', (msg) => targetWs.send(msg));
         targetWs.on('message', (msg) => clientWs.send(msg));
     });
-    
+
     targetWs.on('close', () => clientWs.close());
     clientWs.on('close', () => targetWs.close());
     targetWs.on('error', () => clientWs.close());
     clientWs.on('error', () => targetWs.close());
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('listening on ' + PORT));
